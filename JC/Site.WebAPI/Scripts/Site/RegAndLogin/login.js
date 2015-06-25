e455f4fe5897859556b17d55ee1f9bec37ad993e
junhong <junhong@kooboo.cn>
2015-06-25 20:47:57 +0800

    commit changes

---
+++ JC/Site.WebAPI/Scripts/Site/RegAndLogin/login.js
@@ -0,0 +1,44 @@
+﻿var $btnlogin = $('#btnLogin');
+
+$(function() {
+    $btnlogin.CLICK(function() {
+        $('.middle-box').validate({
+            errorElement: 'span',
+            errorClass: 'help-block',
+            focusInvalid: false,
+            rules: {
+                name: {
+                    required: true
+                },
+                password: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Username is required."
+                },
+                password: {
+                    required: "Password is required."
+                }
+            },
+
+            highlight: function (element) {
+                $(element).closest('.form-group').addClass('has-error');
+            },
+
+            success: function (label) {
+                label.closest('.form-group').removeClass('has-error');
+                label.remove();
+            },
+
+            errorPlacement: function (error, element) {
+                element.parent('div').append(error);
+            },
+
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+    });
+});
